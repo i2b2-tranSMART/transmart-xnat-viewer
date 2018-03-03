87fe627c0462b720a8711bc639d3183323a30b69
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 19:08:15 -0500

    cleanup, version updates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,57 +1,32 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
-grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-]
-
-grails.project.dependency.resolver = "maven" // or ivy
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve true
-    repositories {
-        grailsCentral()
-        mavenLocal()
-        mavenCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-        mavenRepo "https://repo.transmartfoundation.org/content/repositories/public/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile('org.transmartproject:transmart-core-api:16.2-SNAPSHOT')
-        compile 'org.apache.httpcomponents:httpcore:4.4.1'
-        compile 'org.apache.httpcomponents:httpclient:4.4.1'
-        compile ('org.codehaus.groovy.modules.http-builder:http-builder:0.7.1') {
-            excludes('groovy', 'nekohtml', 'httpclient', 'httpcore')
-        }
-    }
 
-    plugins {
-        compile(':search-domain:16.2-SNAPSHOT')
-        build(":release:3.1.1",
-              ":rest-client-builder:2.1.1") {
-            export = false
-        }
-        compile ':scaffolding:1.0.0'
-    }
+	inherits 'global'
+	log 'warn'
+	legacyResolve true
+
+	repositories {
+		mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
+		grailsCentral()
+		mavenCentral()
+		mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
+	}
+
+	dependencies {
+		compile 'org.apache.httpcomponents:httpclient:4.4.1'
+		compile 'org.apache.httpcomponents:httpcore:4.4.1'
+		compile 'org.apache.httpcomponents:httpcore:4.4.1'
+		compile 'org.transmartproject:transmart-core-api:16.2'
+		compile 'org.transmartproject:transmart-core-api:16.2'
+	}
+
+	plugins {
+		compile ':search-domain:18.1-SNAPSHOT'
+
+		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
+			export = false
+		}
+	}
 }
