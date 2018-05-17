2cb980d3285f236327dca00f9d6cc648286a7203
Burt Beckwith <burt@burtbeckwith.com>
2018-05-17 15:41:52 -0400

    fixed the plugin name which was causing problems after upgrading to latest resources plugin

--- grails-app/conf/XNATResources.groovy
+++ grails-app/conf/XNATResources.groovy
@@ -1,19 +1,18 @@
 modules = {
-    xnat {
-        // This should only be depending on jQuery and jQueryUI
-        // But a bug in the resources plugin imposes us to depend on all plugins of the module we overwrite
-        // dependsOn 'jquery', 'jquery-ui'
-        dependsOn 'jquery', 'jquery-ui', 'jquery-plugins', 'extjs', 'session_timeout'
+	xnat {
+		// This should only be depending on jQuery and jQueryUI
+		// But a bug in the resources plugin imposes us to depend on all plugins of the module we overwrite
+		// dependsOn 'jquery', 'jquery-ui'
+		dependsOn 'jquery', 'jquery-ui', 'jquery-plugins', 'extjs', 'session_timeout'
 
-        resource url: [plugin: 'xnat-plugin', dir: 'js/', file: 'xnat.js'], disposition: 'defer'
-        resource url: [plugin: 'xnat-plugin', dir: 'js/', file: 'xnatImage.js'], disposition: 'defer'
-        resource url: [plugin: 'xnat-plugin', dir: 'css/', file: 'xnat.css'], disposition: 'head'
+		resource url: [plugin: 'xnat-viewer', dir: 'js',  file: 'xnat.js'],      disposition: 'defer'
+		resource url: [plugin: 'xnat-viewer', dir: 'js',  file: 'xnatImage.js'], disposition: 'defer'
+		resource url: [plugin: 'xnat-viewer', dir: 'css', file: 'xnat.css'],     disposition: 'head'
+	}
 
-    }
-
-    overrides {
-        analyseTab {
-            dependsOn 'xnat'
-        }
-    }
+	overrides {
+		analyseTab {
+			dependsOn 'xnat'
+		}
+	}
 }
